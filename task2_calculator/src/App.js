84dd608241ad55cefeec8e81e0253e0b0e958fab
gargikanungo <146022238+gargikanungo@users.noreply.github.com>
2023-11-06 19:31:37 +0530

    Add files via upload

---
+++ task2_calculator/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Calculator from "./component/Calculator";
+
+function App() {
+  return (
+    <div className="calculator-grid">
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
